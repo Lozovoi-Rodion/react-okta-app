1e82e66b3a6c6d1147c51810ae2d80e5939b7f87
Rodion Lozovoi <rodionlozovoi@gmail.com>
2019-08-30 07:39:18 +0300

    Simple example of using okta for varification

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route} from "react-router-dom";
+import {Security, SecureRoute, ImplicitCallback} from '@okta/okta-react';
+
+import Navbar from "./components/layout/Navbar";
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
+import OktaLogin from './components/auth/OktaLogin';
+
+const config = {
+    issuer: 'YOUR_DOMAIN',
+    redirect_uri: window.location.origin + '/implicit/callback',
+    client_id: 'YOUR_ID'
+};
+
+function onAuthRequired(history) {
+    history.push('/login')
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Security
+                issuer={config.issuer}
+                client_id={config.client_id}
+                redirect_uri={config.redirect_uri}
+            >
+                <div className="App">
+                    <Navbar />
+                    <div className="container">
+                        <Route path="/" exact={true} component={Home} />
+                        <SecureRoute path="/staff" exact={true} component={Staff} />
+                        <Route
+                            path="/login"
+                            render={() => (
+                                <OktaLogin baseUrl="YOUR_DOMAIN" />
+                            )}
+                        />
+                        <Route path="/implicit/callback" component={ImplicitCallback} />
+                    </div>
+                </div>
+            </Security>
+        </BrowserRouter>
+    );
 }
 
 export default App;
